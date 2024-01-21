648e8d7126a9ebc9b5e90eb883227dc397cbd4b5
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-01-21 18:30:45 +0300

    Redux initialized && Routers added && AxiosWAuth added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import { Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
+import HomePage from "./Layouts/HomePage";
+import BlogListPage from "./Layouts/BlogListPage";
+import BlogPage from "./Layouts/BlogPage";
+import AboutUsPage from "./Layouts/AboutUsPage";
+import UserProfilePage from "./Layouts/UserProfilePage";
+
 function App() {
-  return <div className=" bg-pink-300">test</div>;
+  return (
+    <Switch>
+      <Route exact path={"/"}>
+        <HomePage />
+      </Route>
+      <Route exact path={"/blog"}>
+        <BlogListPage />
+      </Route>
+      <Route exact path={"/blog/:blogId"}>
+        <BlogPage />
+      </Route>
+      <Route exact path={"/about"}>
+        <AboutUsPage />
+      </Route>
+      <Route exact path={"/user"}>
+        <UserProfilePage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
